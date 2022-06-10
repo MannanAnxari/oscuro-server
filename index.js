b250a783e10c1f654f47264b0f1d47f8e7d22997
Mannan <mannananxari@gmail.com>
2022-06-11 02:08:21 +0500

    Backend Updated

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
-const port = process.env.PORT;
+const port = 45 || process.env.PORT;
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-var count = 0;
-const hostname = '0.0.0.0'
-const groupUsers = [{}];
+// var count = 0;
+// const hostname = '0.0.0.0'
+const groupUsers = [{}]; 
 
 app.use(cors());
 app.use("/", (req, res) => {
@@ -17,25 +17,24 @@ app.use("/", (req, res) => {
 })
 
 io.on("connection", (socket) => {
-    console.log("new Connection");
+    // console.log("new Connection");
     socket.on("joined", ({ user }) => {
-        count++;
+        // count++;
         groupUsers[socket.id] = user;
         // console.log(`${user} has Joined`);
-        socket.broadcast.emit("UserJoined", { user: "Admin", message: `${groupUsers[socket.id]} Has Joined` })
+        socket.broadcast.emit("UserJoined", { user: "Admin", message: `${groupUsers[socket.id]} Has Joined` });
         socket.emit('send', { user: "Admin", message: `Welcome To The Chat, ${groupUsers[socket.id]}` });
-        socket.emit('TotalNo', { totalUsers: `${count}` })
-    });
-
-    socket.on('message', ({message, id}) => {
-        io.emit('sendMsg', {user: groupUsers[id],  message, id})
+        // socket.emit('TotalNo', { totalUsers: `${count}` })
+    }); 
+    socket.on('message', ({ message, id }) => {
+        io.emit('sendMsg', { user: groupUsers[id], message, id })
     })
 
     socket.on("disconnect", () => {
-        count--;
+        // count--;
         socket.broadcast.emit("leave", { user: "Admin", message: `${groupUsers[socket.id]} Has Left` })
-        console.log('User Left');
-        socket.emit('TotalNo', { totalUsers: `${count}` })
+        // console.log('User Left');
+        // socket.emit('TotalNo', { totalUsers: `${count}` })
     });
 })
 
